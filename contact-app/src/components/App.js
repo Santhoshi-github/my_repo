8bd7e19305dcabb44a459890140463b6da54ceeb
Santhoshi <santhosha.vanga94@gmail.com>
2023-03-16 22:07:36 -0400

    local-repo for contact-app

---
+++ contact-app/src/components/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './App.css';
+import Header from './Header';
+import AddContact from './AddContact';
+import ContactList from './ContactList';
+
+function App() {
+
+  const contacts = [
+    {
+      id: "1",
+      name: "Santhoshi",
+      email: "santhoshavanga27@gmail.com"
+    },
+    {
+      id: "2",
+      name: "devi",
+      email: "santhosha.vanga94@gmail.com"
+    }
+  ];
+  return (
+   <div className = "ui container">
+    <Header></Header>
+    <AddContact></AddContact>
+    <ContactList contacts = {contacts}></ContactList>
+   </div> 
+  );
+}
+
+export default App;
